b37d7baccb05f912c03682ba5dedf78bd3552009
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2019-11-08 18:04:24 -0500

    Updates

---
+++ precache-manifest.1400ebadb385610a7aafe264d2258cc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d3ead8955c562a11a52ebbaae564ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b04be26152b78afb004b",
+    "url": "/monsters-rolodex/static/css/main.7ea020d4.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "b04be26152b78afb004b",
+    "url": "/monsters-rolodex/static/js/main.548e418e.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
